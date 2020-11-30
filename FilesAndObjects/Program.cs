dff26ef85bf8998e1b0e05781e59f229b07a6855
Opilane TTHK <opilane@tthk.ee>
2020-11-30 15:21:18 +0200

    first commit

---
+++ FilesAndObjects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FilesAndObjects
+{
+    class Program
+    {
+        private static object fileName;
+
+        class Movie
+        {
+            public string title;
+            public string rating;
+            public string year;
+
+
+            public Movie(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            string FilePath = @"C:\Users\opilane\samples";
+            string fileName = @"movietv.txt";
+
+            List<string> movieList = File.ReadAllLines(Path.Combine(FilePath, fileName)).ToList();
+            List<Movie> listOfMovies = new List<Movie>();
+            foreach(string movieItem in movieList)
+            {
+                string[] tempArray = movieItem.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                Movie newMovie = new Movie(tempArray[0], tempArray[1], tempArray[2],);
+
+                listOfMovies.Add(newMovie);
+            }
+
+            foreach(Movie movie in listOfMovies)
+            {
+                Console.WriteLine($"title: {movie.title}; Rating: {movie.rating}; year: {movie.year};");
+            }
+           
+            
+        }
+
+    }
+}
