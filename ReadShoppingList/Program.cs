f911c1a9fe32158717a1207fd29ad01e0707ad87
Opilane TTHK <opilane@tthk.ee>
2020-12-03 12:53:11 +0200

    first commit

--- ReadShoppingList/Program.cs
+++ ReadShoppingList/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-namespace ReadShoppingList
+namespace ReadTotalFromShoppingList
 {
     class Program
     {
@@ -16,25 +16,42 @@ namespace ReadShoppingList
             {
                 name = _name;
                 price = _price;
+                Console.WriteLine($"Item {name} create");
             }
 
             public string Name { get { return name; } }
-            public int Pirce { get { return price; } }
+            public int Price { get { return price; } }
         }
-
         static void Main(string[] args)
         {
             ReadFromShoppingList();
-                
         }
 
         public static void ReadFromShoppingList()
         {
             string filePath = @"C:\Users\opilane\samples";
             string fileName = @"shoppingList.txt";
-            List<Item> shoppingItems = new List<>
-        }
 
-                                                                  
+            List<Item> shoppingItems = new List<Item>();
+
+            List<string> linesFromFile = File.ReadAllLines(Path.Combine(filePath, fileName)).ToList();
+
+            foreach (string line in linesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Item newItem = new Item(tempArray[0],Int32.Parse(tempArray[1]));
+                shoppingItems.Add(newItem);
+
+            }
+
+            Console.WriteLine("your shopping cart:");
+            int total = 0;
+            foreach(Item item in shoppingItems)
+            {
+                Console.WriteLine($"Item: {item.Name}; Price: {item.Price}");
+                total += item.Price;
+            }
+            Console.WriteLine($"Your shopping cart total: {total} ");
+        }
     }
 }
